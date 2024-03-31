eaa5d9c0383a3ddea2207b036df8580fb66e008f
AsrithaRapuri123 <161926768+AsrithaRapuri123@users.noreply.github.com>
2024-03-31 12:20:31 +0530

    tiktaktoe display function

---
+++ tiktaktoe display().py
@@ -0,0 +1,11 @@
+row ="+-------+-------+-------+" 
+column="|       |       |       | \n|       |       |       |  \n|       |       |       |"
+print(row)
+
+def display_board(): 
+    
+    for i in range(3):
+        print(column)
+        print(row)
+    
+display_board() 
